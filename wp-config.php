528bd0531b654bf1452390dfd53c6fbe994746f5
Lion Kutschera <lion.kutschera@edu.fh-joanneum.at>
2018-12-05 12:47:32 +0100

    initial commit

---
+++ wp-config.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Grundeinstellungen für WordPress
+ *
+ * Zu diesen Einstellungen gehören:
+ *
+ * * MySQL-Zugangsdaten,
+ * * Tabellenpräfix,
+ * * Sicherheitsschlüssel
+ * * und ABSPATH.
+ *
+ * Mehr Informationen zur wp-config.php gibt es auf der
+ * {@link https://codex.wordpress.org/Editing_wp-config.php wp-config.php editieren}
+ * Seite im Codex. Die Zugangsdaten für die MySQL-Datenbank
+ * bekommst du von deinem Webhoster.
+ *
+ * Diese Datei wird zur Erstellung der wp-config.php verwendet.
+ * Du musst aber dafür nicht das Installationsskript verwenden.
+ * Stattdessen kannst du auch diese Datei als wp-config.php mit
+ * deinen Zugangsdaten für die Datenbank abspeichern.
+ *
+ * @package WordPress
+ */
+
+// ** MySQL-Einstellungen ** //
+/**   Diese Zugangsdaten bekommst du von deinem Webhoster. **/
+
+/**
+ * Ersetze datenbankname_hier_einfuegen
+ * mit dem Namen der Datenbank, die du verwenden möchtest.
+ */
+define('DB_NAME', 'elearning_plugin_db');
+
+/**
+ * Ersetze benutzername_hier_einfuegen
+ * mit deinem MySQL-Datenbank-Benutzernamen.
+ */
+define('DB_USER', 'root');
+
+/**
+ * Ersetze passwort_hier_einfuegen mit deinem MySQL-Passwort.
+ */
+define('DB_PASSWORD', 'root');
+
+/**
+ * Ersetze localhost mit der MySQL-Serveradresse.
+ */
+define('DB_HOST', 'localhost');
+
+/**
+ * Der Datenbankzeichensatz, der beim Erstellen der
+ * Datenbanktabellen verwendet werden soll
+ */
+define('DB_CHARSET', 'utf8mb4');
+
+/**
+ * Der Collate-Type sollte nicht geändert werden.
+ */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Sicherheitsschlüssel
+ *
+ * Ändere jeden untenstehenden Platzhaltertext in eine beliebige,
+ * möglichst einmalig genutzte Zeichenkette.
+ * Auf der Seite {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * kannst du dir alle Schlüssel generieren lassen.
+ * Du kannst die Schlüssel jederzeit wieder ändern, alle angemeldeten
+ * Benutzer müssen sich danach erneut anmelden.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'u*_{OZb`vnigR|#rlZBUIKY[Rsb{}=t wv&L0%TlGO6/}>YAn(QXVp1LKi>%t#[K');
+define('SECURE_AUTH_KEY',  'RkX:{iT)5Zd|=rc<dNyIS!Rr#&#=SI-zo:1;0YkZ&Ij&ExGKtvT?I{O3~8FS7w>N');
+define('LOGGED_IN_KEY',    'sZS|n/iD;!|Z}.%k?*uW J0S&savIvufF*Je:XPyPV10,4Dkn(>9/`-Jdp0mI#AU');
+define('NONCE_KEY',        'nw>i:>P`3h|RwIif5}Zhg9V)I7g0f60VO5]ZCN|rH]Y@W2w:|3  4mDKpD6`,W]D');
+define('AUTH_SALT',        'r?5UER^O24Jm)JcuaE&A7354mh:n[{tVfl(.{O{!eE|%Qzn~Nn4zYlciRcU9E )5');
+define('SECURE_AUTH_SALT', '.PD0!gtMjnu>;s-IA|@k-z:{|fs3o (-L;mm8~DJ %{6:w$lS8>dFzzF;YPLw)ol');
+define('LOGGED_IN_SALT',   'co_[{|X.FU@7_7H8>|0g^%yYhH4-.=@2bDZA|-/7Jr9q>:loXe5b`AZ{PPqT^EGd');
+define('NONCE_SALT',       '(9[d(DfY3$W2AH(Tg1xGOZ{Z8.O_>JDh![@xJ|w8YEuUrMplosw>65:)3Y:v# 8Q');
+
+/**#@-*/
+
+/**
+ * WordPress Datenbanktabellen-Präfix
+ *
+ * Wenn du verschiedene Präfixe benutzt, kannst du innerhalb einer Datenbank
+ * verschiedene WordPress-Installationen betreiben.
+ * Bitte verwende nur Zahlen, Buchstaben und Unterstriche!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * Für Entwickler: Der WordPress-Debug-Modus.
+ *
+ * Setze den Wert auf „true“, um bei der Entwicklung Warnungen und Fehler-Meldungen angezeigt zu bekommen.
+ * Plugin- und Theme-Entwicklern wird nachdrücklich empfohlen, WP_DEBUG
+ * in ihrer Entwicklungsumgebung zu verwenden.
+ *
+ * Besuche den Codex, um mehr Informationen über andere Konstanten zu finden,
+ * die zum Debuggen genutzt werden können.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* Das war’s, Schluss mit dem Bearbeiten! Viel Spaß beim Bloggen. */
+/* That's all, stop editing! Happy blogging. */
+
+/** Der absolute Pfad zum WordPress-Verzeichnis. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Definiert WordPress-Variablen und fügt Dateien ein.  */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/plugins/elearning-teacher-student-plugin/elearning-teacher-student-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package ElearningTeacheStudentPlugin
+ */
+
+/*
+Plugin Name: elearning Teacher Student Plugin
+Plugin URI: https://github.com/lioncay/elearning-teacher-student-plugin.git
+Description: elearning plugin for creating courses and see attempt
+Version: 1.0.0
+Author: lioncay
+Author URI: https://github.com/lioncay
+License: GPLv2 or later
+Text Domain: elearning-teacher-student-plugin
+ */
