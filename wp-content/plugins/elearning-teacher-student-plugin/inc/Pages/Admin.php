553e58df8ab64690d54c943fec060e31152e5bcf
Lion Kutschera <lion.kutschera@edu.fh-joanneum.at>
2018-12-14 07:14:25 +0100

    theme updates and tried to add new page on user side

--- wp-content/plugins/elearning-teacher-student-plugin/inc/Pages/Admin.php
+++ wp-content/plugins/elearning-teacher-student-plugin/inc/Pages/Admin.php
@@ -6,27 +6,42 @@
 
 namespace Inc\Pages;
 
-class Admin{
+class Admin
+{
 
-    public function register(){
+    public function register()
+    {
         add_action('admin_menu', array($this, 'add_admin_pages'));
+        add_action('admin_menu', 'MyPluginPage');
     }
 
-    public function add_admin_pages(){
+    public function add_admin_pages()
+    {
         add_menu_page(
             'Elearning Plugin',
             'Elearning',
             'manage_options',
             'elearning_plugin',
-            array($this,'admin_index'),
+            array($this, 'admin_index'),
             'dashicons-welcome-learn-more',
             110
         );
     }
 
-    public function admin_index(){
-        require_once PLUGIN_PATH.'templates/admin.php';
+    public function admin_index()
+    {
+        require_once PLUGIN_PATH . 'templates/admin.php';
 
+    }
+
+    function MyPluginPage()
+    {
+        $new_post = array(
+            'post_title' => 'Draft title',
+            'post_status'=> 'draft',
+            'post_type'  => 'my_custom_type'
+        );
+        wp_insert_post($new_post,$wp_error = false );
     }
 
 }
