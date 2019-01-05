70d86346a425040002cab6de7978d43d910972df
Lion Kutschera <lion.kutschera@edu.fh-joanneum.at>
2019-01-06 00:38:23 +0100

    style update(courses page). delete courses(just the page. now possible)

--- wp-content/plugins/elearning-teacher-student-plugin/inc/Pages/courses.php
+++ wp-content/plugins/elearning-teacher-student-plugin/inc/Pages/courses.php
@@ -1,7 +1,12 @@
 <?php
 global $wpdb;
 
-echo '<form><button type="button" onclick="document.location.href=\''.get_home_url().'/add-course\'">Create new Course</button></form>';
+if(isset($_GET['id'])){
+    wp_delete_post($_GET['id'], true);
+    echo "<script>window.location = window.location.pathname;</script>";
+}
+
+echo '<button type="submit" onclick="document.location.href=\''.get_home_url().'/add-course\'">Create new Course</button>';
 
 $query = $wpdb->prepare(
     'SELECT `post_id` FROM ' . $wpdb->postmeta . ' WHERE `meta_value` IN (SELECT ID FROM ' . $wpdb->posts . ' WHERE post_title = %s) LIMIT 1',
@@ -13,24 +18,27 @@ $id = $wpdb->last_result[0]->post_id;
 
 global $wpdb;
 $query = $wpdb->prepare(
-    'SELECT `post_title` FROM ' . $wpdb->posts . ' WHERE `ID` IN (SELECT `meta_value` FROM ' . $wpdb->postmeta . ' WHERE `meta_key`= \'_menu_item_object_id\' && `post_id` IN (SELECT `post_id` FROM ' . $wpdb->postmeta . ' WHERE `meta_value` = %s AND `meta_key`=\'_menu_item_menu_item_parent\'))',
+    'SELECT `post_title`,`ID` FROM ' . $wpdb->posts . ' WHERE `ID` IN (SELECT `meta_value` FROM ' . $wpdb->postmeta . ' WHERE `meta_key`= \'_menu_item_object_id\' && `post_id` IN (SELECT `post_id` FROM ' . $wpdb->postmeta . ' WHERE `meta_value` = %s AND `meta_key`=\'_menu_item_menu_item_parent\'))',
     $parentid = '' . $id
 );
 $wpdb->query($query);
 if ( $wpdb->num_rows ) {
     $items = $wpdb->last_result;
-    $string = '<ul>';
+    $string = '<table class="ulcomponents">';
     foreach ($items as $item) {
-        $string .= '<a href="' . get_home_url() . '/'.$item->post_title.'"><li class="">';
-        $string .= $item->post_title;
-        $string .= '</li></a>';
+        $string .= '<a href="' . get_home_url() . '/'.$item->post_title.'"><tr id="trofulcomponents">';
+        $string .= '<td class="lefttitle">' . $item->post_title . '</td><td class="rightaction">
+                        <div class="paper_basket_icon" onclick="location.href=\'all-courses?id=' . $item->ID . '\';"></div>
+                        <div class="edit_icon" onclick="location.href=\'pageurl.html\';"></div>
+                    </td>';
+        $string .= '</tr></a>';
     }
 
-    $string .= '</ul>';
+    $string .= '</table>';
     $string .= '';
     $string .= '';
     echo $string;
 }else{
-    echo 'Es wurden noch keine Kurse erstellt!';
+    echo '<p>Es wurden noch keine Kurse erstellt!</p>';
 }
 ?>
