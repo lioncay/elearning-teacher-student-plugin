4200b0dcd8d7c2be5e6d25739b66a44e6d7d4785
Lion Kutschera <lion.kutschera@edu.fh-joanneum.at>
2019-02-14 03:04:57 +0100

    courses now listet for users

--- wp-content/plugins/elearning-teacher-student-plugin/inc/Pages/Admin.php
+++ wp-content/plugins/elearning-teacher-student-plugin/inc/Pages/Admin.php
@@ -249,6 +249,94 @@ class Admin
 
 
         }
+        $query = $wpdb->prepare(
+            'SELECT ID FROM ' . $wpdb->posts . ' WHERE post_title = %s',
+            $postTitle="Meine Kurse"
+        );
+        $wpdb->query( $query );
+        if ( $wpdb->num_rows ) {
+
+        }else{
+            $new_post = array(
+                'post_title' => "Meine Kurse",
+                'post_name' => "my-courses",
+                'post_status'=> 'publish',
+                'post_type'  => 'page',
+                'post_content' => '[php_everywhere]',
+                'post_author' => '1',
+                'post_category' => array(1,2),
+                'page_template' => NULL
+            );
+            wp_insert_post($new_post,$wp_error=false);
+            $query = $wpdb->prepare(
+                'SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_NAME = %s',
+                $table = $wpdb->posts
+            );
+            $wpdb->query( $query );
+            $id=$wpdb->last_result[0]->AUTO_INCREMENT;
+            $wpdb->query( $query );
+            $query = $wpdb->prepare(
+                'SELECT `count` FROM ' . $wpdb->term_taxonomy . ' WHERE `term_taxonomy_id` = %d',
+                $taxid = 2
+            );
+            $wpdb->query( $query );
+            $menuorder=$wpdb->last_result[0]->count+1;
+            $menu = array(
+                'post_status'=> 'publish',
+                'post_type'  => 'nav_menu_item',
+                'post_author' => '1',
+                'guid' => get_home_url().'/?p='.$id,
+                'menu_order' => $menuorder,
+                'post_name' => $id,
+                'comment_status' => "closed",
+                'ping_status' => "closed"
+            );
+            wp_insert_post($menu,$wp_error=false);
+            $query = $wpdb->prepare(
+                'INSERT INTO ' . $wpdb->term_relationships . ' (`object_id`, `term_taxonomy_id`, `term_order`) VALUES (%d,%d,%d)',
+                $one=$id,
+                $two=2,
+                $three=0
+            );
+            $wpdb->query( $query );
+            $query = $wpdb->prepare(
+                'UPDATE ' . $wpdb->term_taxonomy . ' SET `count` = `count` + 1 WHERE `term_taxonomy_id` = %d',
+                $taxid = 2
+            );
+            $wpdb->query( $query );
+
+            $query = $wpdb->prepare(
+                'SELECT ID FROM ' . $wpdb->posts . ' WHERE post_title = %s',
+                $postTitle="Meine Kurse"
+            );
+            $wpdb->query( $query );
+            $postid = $wpdb->last_result[0]->ID;
+
+            $this->do_insert($id,'_menu_item_type','post_type');
+            $this->do_insert($id,'_menu_item_menu_item_parent','0');
+            $this->do_insert($id,'_menu_item_object_id','' . $postid . '');
+            $this->do_insert($id,'_menu_item_object','page');
+            $this->do_insert($id,'_menu_item_target','');
+            $this->do_insert($id,'_menu_item_classes','a:1:{i:0;s:0:"";}');
+            $this->do_insert($id,'_menu_item_xfn','');
+            $this->do_insert($id,'_menu_item_url','');
+            $this->do_insert($id,'_menu_item_template','');
+            $this->do_insert($id,'_menu_item_mega_template','0');
+            $this->do_insert($id,'_menu_item_nolink','');
+            $this->do_insert($id,'_menu_item_category_post','');
+            $this->do_insert($id,'_menu_item_megamenu','');
+            $this->do_insert($id,'_menu_item_megamenu_auto_width','');
+            $this->do_insert($id,'_menu_item_megamenu_col','');
+            $this->do_insert($id,'_menu_item_megamenu_heading','');
+            $this->do_insert($id,'_menu_item_megamenu_widgetarea','0');
+            $this->do_insert($id,'_menu_item_icon','');
+
+            $string = file_get_contents('my_courses.php', TRUE);
+            $courses = "" . $string;
+            $this->do_insert($postid,'php_everywhere_code',$courses);
+
+
+        }
     }
 
     function do_insert($postid, $second, $third){
---
+++ wp-content/plugins/elearning-teacher-student-plugin/inc/Pages/my_courses.php
@@ -0,0 +1,27 @@
+<?php
+global $wpdb;
+
+$query = $wpdb->prepare(
+    'SELECT `name`,id FROM courses WHERE pageid IN (SELECT courseid FROM users_of_course WHERE userid = %s)',
+    $id = get_current_user_id()
+);
+$wpdb->query($query);
+if ( $wpdb->num_rows ) {
+    $items = $wpdb->last_result;
+    $string = '<table class="ulcomponents">';
+    foreach ($items as $item) {
+        $string .= '<tr id="trofulcomponents">';
+        $string .= '<td class="lefttitle" onclick="location.href=\'' . get_home_url() . '/'.$item->id.'\'">' . $item->name . '</td><td class="rightaction">
+                        <div class="startcourse" onclick="location.href=\'#\'"></div>
+                    </td>';
+        $string .= '</tr>';
+    }
+
+    $string .= '</table>';
+    $string .= '';
+    $string .= '';
+    echo $string;
+}else{
+    echo '<h1 style="text-align: center;">Du wurdest leider noch keinen Kursen zugeteilt!</h1>';
+}
+?>
