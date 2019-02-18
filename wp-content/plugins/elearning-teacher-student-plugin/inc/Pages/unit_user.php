0cd0cbf6e4f50273cff5ffb054acb220ddcb9237
Lion Kutschera <lion.kutschera@edu.fh-joanneum.at>
2019-02-18 16:41:07 +0100

    created new list for current chapterentry

--- wp-content/plugins/elearning-teacher-student-plugin/inc/Pages/unit_user.php
+++ wp-content/plugins/elearning-teacher-student-plugin/inc/Pages/unit_user.php
@@ -2,10 +2,11 @@
 global $wpdb;
 $coursedetails = array();
 $counter= 0;
+$done=false;
 if (isset($_GET['unitname'])){
     echo "<script>document.getElementsByTagName('h1')[0].innerHTML = ' Unit - " . $_GET['unitname'] . "'</script>";
 }
-if (!isset($_POST['previous'])){
+if (!isset($_POST['counter'])){
     $query = $wpdb->prepare(
         'SELECT * FROM chapters WHERE unit_id = %d',
         $id = $_GET['unitid']
@@ -39,12 +40,19 @@ if (!isset($_POST['previous'])){
     }
     $listofcoursdetails .= "</ol>";
     echo "<script>window.onload = function what(){document.getElementById('right-sidebar').innerHTML = \"" . $listofcoursdetails . "\"};</script>";
-}else if (isset($_POST['previous'])){
+}else if (isset($_POST['counter'])){
     $counter = $_POST['counter'];
-    $coursedetails = $_POST['coursedetails'];
+    $tmp = explode("&",$_POST['coursedetails']);
+    $coursedetails = array();
+    foreach($tmp as $item){
+        array_push($coursedetails,explode("-",$item));
+    }
     $listofcoursdetails = "<ol>";
     $state=0;
     $lastcoursedetail=false;
+    if (sizeof($coursedetails)==$counter+1){
+        $done=true;
+    }
     foreach ($coursedetails as $item) {
         if (sizeof($item)<3){
             if ($lastcoursedetail){
@@ -57,7 +65,11 @@ if (!isset($_POST['previous'])){
             }
         }else{
             $listofcoursdetails .= "<ol>";
-            $listofcoursdetails .= "<li>" . $item[0] . "</li>";
+            if ($counter>=$state){
+                $listofcoursdetails .= "<li class='blackstate'>" . $item[0] . "</li>";
+            }else{
+                $listofcoursdetails .= "<li class='graystate'>" . $item[0] . "</li>";
+            }
         }
         $lastcoursedetail=true;
         $state++;
@@ -78,6 +90,72 @@ if (isset($coursedetails[$counter])){
                 echo $wpdb->last_result[0]->summary;
             }
         }
+    }else{
+        $query = $wpdb->prepare(
+            'SELECT * FROM chapterentries WHERE id = %d',
+            $id = $coursedetails[$counter][1]
+        );
+        $wpdb->query($query);
+        if($wpdb->num_rows){
+            if($wpdb->last_result[0]->entry_type=="IN"){
+                echo "INFO";
+            }elseif ($wpdb->last_result[0]->entry_type=="MC"){
+                echo "Multiple choice";
+            }elseif ($wpdb->last_result[0]->entry_type=="OQ"){
+                echo "Open Question";
+            }
+        }
+    }
+    $string = "";
+    foreach ($coursedetails as $item){
+        foreach ($item as $inneritem){
+            $string .= $inneritem . "-";
+        }
+        $string = substr($string, 0, -1);
+        $string .= "&";
+    }
+    $string = substr($string, 0, -1);
+    if (!$done) {
+        ?>
+        <form method="post">
+            <input type="hidden" name="coursedetails" value="<?php echo $string; ?>">
+            <input type="hidden" name="counter" value="<?php echo $counter + 1; ?>">
+            <button type="submit" name="submit">Weiter</button>
+        </form>
+        <?php
+    }else{
+        $query = $wpdb->prepare(
+            'SELECT * FROM user_course_attempts WHERE userid = %d AND courseid = %d',
+            $user=get_current_user_id(),
+            $courseid = $_GET['courseid']
+        );
+        $wpdb->query($query);
+        if($wpdb->num_rows){
+            print_r($wpdb->last_result[0]);
+            if ($wpdb->last_result[0]->done_units!=""){
+                $newdone_units = $wpdb->last_result[0]->done_units . "," . $_GET['unitid'];
+            }else{
+                $newdone_units = $_GET['unitid'];
+            }
+            $query = $wpdb->prepare(
+                'UPDATE `user_course_attempts` SET `done_units`=%s WHERE userid=%d AND courseid=%d',
+                $one= $newdone_units,
+                $two=get_current_user_id(),
+                $three=$_GET['courseid']
+            );
+            $wpdb->query( $query );
+        }
+        $query = $wpdb->prepare(
+            'SELECT pageid FROM courses WHERE id = %d',
+            $courseid = $_GET['courseid']
+        );
+        $wpdb->query($query);
+        ?>
+        <form method="post" action="<?php echo get_home_url(); ?>/course/?courseid=<?php echo $wpdb->last_result[0]->pageid; ?>">
+            <input type="hidden" value="Ja" name="confirm">
+            <button type="submit">Beenden</button>
+        </form>
+        <?php
     }
 }
 ?>
